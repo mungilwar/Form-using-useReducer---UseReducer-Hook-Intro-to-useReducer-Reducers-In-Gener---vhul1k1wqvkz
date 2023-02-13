0e41c0f6226a49881996019e50f4dd33f41a83b6
mungilwar <nitinmungilwar4@gmail.com>
2023-02-13 14:23:07 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
-import React from 'react'
+import React from 'react';
 import '../styles/App.css';
 import { FormUsingReducer } from './FormUsingReducer';
 const App = () => {
-
   return (
     <div id="main">
       <FormUsingReducer />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
